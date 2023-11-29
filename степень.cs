b8a793c4dc26e0e0f8f24f21fef64fe3fa5cf187
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-11-29 12:07:58 +0500

    Чел вводит 2 числа и от 1 до 2 числа каждое возводится в степень, да

---
+++ степень.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+double x = Convert.ToDouble(Console.ReadLine());
+double r = 1;
+Console.Write("Введите степень: ");
+int y = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < y; i++)
+{
+    r *= x;
+}
+Console.WriteLine(r);
