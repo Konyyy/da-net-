4e3fb192400a5f7dcc4efcf754c24608e592acb3
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-11-29 11:19:30 +0500

    Чел вводит 2 числа и надо ему все перечислить от первого до второго, понятно?

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите 1 число ");
+
+int x =Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine("Введите 2 число ");
+
+int y = Convert.ToInt32(Console.ReadLine());
+
+for (int i = x; i <= y; i++) 
+
+{
+
+    Console.WriteLine(i);
+
+}
+
