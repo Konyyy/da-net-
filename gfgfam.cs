c33f15913b89199a4e8ce378b68dcfab80b29e8a
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-12-01 11:15:59 +0500

    челик вводит число x и y и эта прога считает  сумму всех чисел (т.е. чел ввёл 1  и 3 ( 1 + 2 + 3 = 6))

---
+++ gfgfam.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Введите 1 число(до 100) ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число(до 100) ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 100)
+
+    Console.WriteLine("ОШИБКА");
+else if (y > 100)
+    Console.WriteLine("ОШБИКА");
+else {
+    int sum;
+    sum = 0;
+    while (x <= y)
+    {
+        sum = sum + x;
+        x++;
+    }
+    Console.WriteLine($"Сумма всех чисел = {sum}");
+}
+
