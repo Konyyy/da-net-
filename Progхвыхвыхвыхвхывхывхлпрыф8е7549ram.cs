172f4a806e6d52868d332c9bf69d1927592d6b13
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-12-01 11:53:23 +0500

    Чел такой вводит числа пока сумма не будет равна 100 и после выводит сколько цифр он ввёл ну типа введено цифр 6 а не результат помогите меня держат в заложниках

---
+++ Progхвыхвыхвыхвхывхывхлпрыф8е7549ram.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Вводите любые числа, а после того как сумма всех чисел достигнет 100 программа прекратит работу ");
+
+int sum = 0;
+int f = 0;
+    while (sum < 100)
+    {
+    Console.WriteLine("Введите  число пока сумма не привысит 100");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    sum += num;
+    f++;
+   
+    }
+    
+    Console.WriteLine("Сумма привышает 100 введенно цифр  "+ f );
+
