7c47783ead5a58b142f45f8e4b2203cf65e76427
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-12-01 11:32:35 +0500

    Код запрашивает у человека любые цифры и если введёт 0 то он посчитает сумму всех ранее введенных чисел

---
+++ Pааааааram.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Вводите любые числа, а после того как законичите введите 0 и программа посчитает сумму всех чисел которые вы ввели ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (num != 0)
+{
+    sum += num;
+    Console.WriteLine("Введите ещё число а если хотите закончить введите число 0");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(sum);
+
