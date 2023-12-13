0d7ca197e15d1b2b50c8ea4d188f8649e5a5c2e6
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-12-13 11:29:16 +0500

    чел вводит число от и до и пока не сгенерирует ,короче поймёте

---
+++ 96134146642643971143679147369243268ram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите начало диапазона: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите конец диапазона: ");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число при котором программа остановится: ");
+        int z = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+        int generatedNumber;
+
+        do
+        {
+            generatedNumber = random.Next(x, y + 1);
+            Console.WriteLine("Сгенерированное число: " + generatedNumber);
+        }
+        while (generatedNumber != z);
+
+        Console.WriteLine("Сгенерировано число " + z + ".");
+    }
+}
