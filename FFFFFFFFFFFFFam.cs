450df246ac7de711b062b759b09d66f971b52383
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-12-13 11:58:37 +0500

    schitaet chisla ne sifry

---
+++ FFFFFFFFFFFFFam.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int sn = GetDigitCount(number);
+
+        Console.WriteLine($"Количество цифр в числе: {sn}");
+;
+    }
+
+    static int GetDigitCount(int number)
+    {
+        int i = 0;
+
+       
+        if (number == 0)
+        {
+            i = 1;
+        }
+        else
+        {
+       
+            if (number < 0)
+            {
+                number = -number;
+            }
+
+
+            while (number > 0)
+            {
+                number /= 10;
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
