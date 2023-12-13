c7e03b629bb2dafb0d8a70d401133fbd0b028cc6
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-12-13 11:17:28 +0500

    так пользователь вводит пароль если он его правильно  повторяет то он молодец если нет то он тупой и если stop ввести программа остановится

---
+++ poroly ne 1488.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+
+    {
+        Console.Write("Введите пароль: ");
+        string password = Console.ReadLine();
+        while (true)
+        {
+
+            Console.Write("Повторите пароль: ");
+            string repeatPassword = Console.ReadLine();
+
+            while (password != repeatPassword)
+            {
+                if (repeatPassword.ToLower() == "stop")
+                {
+                    break;
+                }
+                Console.WriteLine("Пароли не совпадают. Повторите попытку или введите 'Stop' для выхода.");
+                repeatPassword = Console.ReadLine();
+            }
+
+            if (repeatPassword.ToLower() == "stop")
+            {
+               break;
+            }
+
+            Console.WriteLine("Пароль успешно введён!");
+            break;
+        }
+    }
+}
