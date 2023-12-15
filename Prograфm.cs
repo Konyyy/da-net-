1b23a1603614b19a98644dbdd07220bb0ab78fc2
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-12-15 11:27:57 +0500

    xtk ltkftn vfccbd f gjckt pfgjkyxtn tuj

---
+++ Prograфm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер массива: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[size];
+
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Введенный массив:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.WriteLine($"Элемент {i + 1}: {array[i]}");
+        }
+    }
+}
