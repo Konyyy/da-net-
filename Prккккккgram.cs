75a30c984d025f58a41121f04ca83421ead973c4
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-12-15 11:51:31 +0500

    сумма всех чисел в массиве которые ввёл пользователь тоесть ты

---
+++ Prккккккgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер массива: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[size];
+
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int sum = 0;
+        foreach (int number in array)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"Сумма всех чисел в массиве: {sum}");
+    }
+}
