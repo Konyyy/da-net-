30afe18a5af790902a1be37ce3e421714af2bb2a
Konyyy <151906976+Konyyy@users.noreply.github.com>
2023-12-15 11:37:27 +0500

    чел делает массив и прога считавает кол-во отрицательнвх и положительных чисел

---
+++ АААААФАЫФАФАЦАФЫАm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер массива: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        int[] da = new int[x];
+
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < x; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            da[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int polozytelyno = 0;
+        int otrizatelyno = 0;
+
+        foreach (int number in da)
+        {
+            if (number > 0)
+                polozytelyno++;
+            else if (number < 0)
+                otrizatelyno++;
+        }
+
+        Console.WriteLine($"Количество положительных чисел: {polozytelyno}");
+        Console.WriteLine($"Количество отрицательных чисел: {otrizatelyno}");
+    }
+}
